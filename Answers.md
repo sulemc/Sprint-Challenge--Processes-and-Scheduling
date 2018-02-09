4fd254af6abf1b8c4cfc071582ad0415333bde13
sulemc <susanna.mcdo@gmail.com>
2018-02-09 15:54:30 -0600

    that's as good as it's gonna git

--- Answers.md
+++ Answers.md
@@ -1,10 +1,16 @@
 1.
 
 
-2. 
+2. Created - a process is created and awaits being admitted to the 'ready' state by the scheduler.
+    Ready - the process has been loaded into memory and awaits execution.
+    Running - the process is being executed
+    Blocked - the process needs something else to happen before it can continue (ie user input)
+    Terminated - the process has finished or is killed by the user.
 
 
 3. 
 
 
 4.
+
+//white flag.
---
+++ ex1/ex1.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void) {
+  int *alloc_1d(int cols)
+{
+  int *A;
+ return	A = (int*) malloc(cols * sizeof(int));
+}
+
+void alloc_1d_example(void)
+{
+	int *my_array = alloc_1d(12);
+	my_array[8] = 3490;
+	// The same array access could be made with pointer arithmetic:
+	*(my_array+8) = 3490;
+
+	// By the C spec, the array notation and pointer arithmetic notation
+	// are 100% equivalent.
+
+	printf("my_array[8] = %d\n", my_array[8]);
+}
+}
